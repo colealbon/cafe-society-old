7b621f2e5083b62f9279b2cce360792ab22c32bd
Cole Albon <cole@cafe-society.news>
2023-06-22 15:01:37 -0700

    troubleshoot deploy

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,7 @@ const App: Component = () => {
   return (
     <div class={styles.App}>
       <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+          cafe-society - soon
       </header>
     </div>
   );
