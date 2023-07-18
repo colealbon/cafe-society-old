54298862e8903c244f45db5ea8b6a99b9e24bacf
Cole Albon <cole@cafe-society.news>
2023-07-17 17:09:33 -0700

    new fields in classifiers

--- src/defaultClassifiers.ts
+++ src/defaultClassifiers.ts
@@ -1,14 +1,14 @@
 const defaultClassifiers = [
-  { "id": "maritime", "thresholdSuppressDocCount": "500", "thresholdPromoteDocCount": "500", "model": "{\"classifier\":{\"classFeatures\":{},\"classTotals\":{},\"totalExamples\":1,\"smoothing\":1},\"docs\":[],\"features\":{},\"stemmer\":{},\"lastAdded\":0,\"events\":{\"domain\":null,\"_events\":{},\"_eventsCount\":0}}" },
-  { "id": "work", "thresholdSuppressDocCount": "500", "thresholdPromoteDocCount": "500", "model": "" },
-  { "id": "science", "thresholdSuppressDocCount": "500", "thresholdPromoteDocCount": "500", "model": "" },
-  { "id": "bitcoin", "thresholdSuppressDocCount": "500", "thresholdPromoteDocCount": "500", "model": "" },
-  { "id": "local", "thresholdSuppressDocCount": "500", "thresholdPromoteDocCount": "500", "model": "" },
-  { "id": "business", "thresholdSuppressDocCount": "500", "thresholdPromoteDocCount": "500", "model": "" },
-  { "id": "world", "thresholdSuppressDocCount": "500", "thresholdPromoteDocCount": "500", "model": "" },
-  { "id": "politics", "thresholdSuppressDocCount": "500", "thresholdPromoteDocCount": "500", "model": "" },
-  { "id": "technology", "thresholdSuppressDocCount": "500", "thresholdPromoteDocCount": "500", "model": "" },
-  { "id": "variety", "thresholdSuppressDocCount": "500", "thresholdPromoteDocCount": "500", "model": "" },
-  { "id": "medicine", "thresholdSuppressDocCount": "500", "thresholdPromoteDocCount": "500", "model": "" }
+  { "id": "maritime", "thresholdSuppressDocCount": "500", "thresholdPromoteDocCount": "500", "model": "{\"classifier\":{\"classFeatures\":{},\"classTotals\":{},\"totalExamples\":1,\"smoothing\":1},\"docs\":[],\"features\":{},\"stemmer\":{},\"lastAdded\":0,\"events\":{\"domain\":null,\"_events\":{},\"_eventsCount\":0}}", "thresholdSuppressOdds": "","thresholdPromoteOdds": "" },
+  { "id": "work", "thresholdSuppressDocCount": "500", "thresholdPromoteDocCount": "500", "model": "", "thresholdSuppressOdds": "","thresholdPromoteOdds": "" },
+  { "id": "science", "thresholdSuppressDocCount": "500", "thresholdPromoteDocCount": "500", "model": "", "thresholdSuppressOdds": "","thresholdPromoteOdds": "" },
+  { "id": "bitcoin", "thresholdSuppressDocCount": "500", "thresholdPromoteDocCount": "500", "model": "", "thresholdSuppressOdds": "","thresholdPromoteOdds": "" },
+  { "id": "local", "thresholdSuppressDocCount": "500", "thresholdPromoteDocCount": "500", "model": "", "thresholdSuppressOdds": "","thresholdPromoteOdds": "" },
+  { "id": "business", "thresholdSuppressDocCount": "500", "thresholdPromoteDocCount": "500", "model": "", "thresholdSuppressOdds": "","thresholdPromoteOdds": "" },
+  { "id": "world", "thresholdSuppressDocCount": "500", "thresholdPromoteDocCount": "500", "model": "", "thresholdSuppressOdds": "","thresholdPromoteOdds": "" },
+  { "id": "politics", "thresholdSuppressDocCount": "500", "thresholdPromoteDocCount": "500", "model": "", "thresholdSuppressOdds": "","thresholdPromoteOdds": "" },
+  { "id": "technology", "thresholdSuppressDocCount": "500", "thresholdPromoteDocCount": "500", "model": "", "thresholdSuppressOdds": "","thresholdPromoteOdds": "" },
+  { "id": "variety", "thresholdSuppressDocCount": "500", "thresholdPromoteDocCount": "500", "model": "", "thresholdSuppressOdds": "","thresholdPromoteOdds": "" },
+  { "id": "medicine", "thresholdSuppressDocCount": "500", "thresholdPromoteDocCount": "500", "model": "", "thresholdSuppressOdds": "","thresholdPromoteOdds": ""}
 ]
 export default defaultClassifiers
