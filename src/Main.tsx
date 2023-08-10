07a94e688914a320295c8ed7c6c878fe51ebbef3
Cole Albon <cole@cafe-society.news>
2023-08-09 17:59:47 -0700

    less css --> more tailwind

--- src/Main.tsx
+++ src/Main.tsx
@@ -4,15 +4,13 @@ const Main = (props) => {
   const c = children(() => props.children);
   return (
     <div
-      class='maintransition'
+      class='fade-in'
       style={{
-      'margin-left': `${props.isOpen() === true ? props.navBarWidth : 0}px`
-    }}>
-    <div class='main' style={{"margin-left": "10px"}}>
-      <div style={{'display':'flex', 'flex-direction':'column'}}>
+        'margin-left': `${props.isOpen() === true ? props.navBarWidth : 0}px`
+      }}>
+    <div class="transition duration-300 ease-in-out mr-10 ml-10 flex flex-col">
       {c()}
       </div>
-    </div>
   </div>
   )
 }
